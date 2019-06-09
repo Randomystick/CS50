29ccd7198cbb95232f6d12e7d9af92b8ccd9b8bd
Bryan Leow <leow.kenhing.bryan@DHS.SG>
2019-06-09 15:17:28 +0800

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[])
 {
     // VALIDATING THE INPUT
     int KEY = 0;
-    int isValid = 1;
+    bool isValid = 1;
     if (argc != 2) 
     {
         isValid = 0;
@@ -22,43 +22,44 @@ int main(int argc, string argv[])
         {
             if ( !(isdigit(argv[1][i])) ) 
             {
-                isValid--;
+                isValid = 0;
             }
         }
     }
 
-    switch(isValid) {
-        case 1:
-            KEY = atoi(argv[1]);
-            break;
-        default:
-            printf("Usage: ./caesar key\n");
-            break;
+    if (isValid)
+    {
+        KEY = atoi(argv[1]);
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
     } 
     // VALIDATING INPUT END
     
     //PRINTING THE ENCRYPTED TEXT
-    if (isValid == 1) {
+    if (isValid) {
         string text = get_string("plaintext: ");
         printf("ciphertext: ");
         for (int i = 0, n=strlen(text); i < n; i++) 
         {
             if (text[i] >= 65 && text[i] <= 90) //Capital A to Z
             {
-                int textInt = (text[i] + (KEY%26));
-                if (textInt > 90) { textInt = textInt % 91 + 65; }
+                int textInt = (text[i] + (KEY%26)); //Ensure text+KEY doesn't exceed 90 by more than 1 iteration of A-Z (ie 26)
+                if (textInt > 90) { textInt = textInt % 91 + 65; } //Normalise textInt to be within A-Z
                 printf("%c", textInt);
             }
-            else if (text[i] >= 97 && text[i] <= 122) 
+            else if (text[i] >= 97 && text[i] <= 122) //Same thing for small a-z
             {
                 int textInt = (text[i] + (KEY%26));
                 if (textInt >= 123) { textInt = textInt % 123 + 97; }
                 printf("%c", textInt);
             }
-            else { printf("%c", text[i]); }
+            else { printf("%c", text[i]); } //punctuations
         }
-        printf("\n");
+        printf("\n"); //endline
     }
     
     return 0;
 }
+
